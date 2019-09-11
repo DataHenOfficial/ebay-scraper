34e190ba7ddb3a5272f6f34f241c3ae648942d84
Parama Danoesubroto <parama@answersengine.com>
2019-09-11 16:15:52 -0400

    added input vars

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -7,5 +7,9 @@ outputs << {
   _collection: 'env_vars',
   _id: 'seeder',
   start_url: ENV['start_url'],
-  somesecret: ENV['somesecret']
+  secret1: ENV['secret1'],
+  date1: ENV['date1'],
+  datetime1: ENV['datetime1'],
+  string1: ENV['string1'],
+  text1: ENV['text1']
 }
