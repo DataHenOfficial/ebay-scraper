455f7c5f4fad2cb89e77cbd15d1e1e4ba60ab293
Parama Danoesubroto <parama@answersengine.com>
2019-10-02 19:35:37 -0400

    Adding search input var

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,5 +1,13 @@
+require 'uri'
+
+@search = ENV['search']
+
+unless @search
+  raise "Need to specify search query"
+end
+
 pages << {
     page_type: 'listings', 
     method: "GET",
-    url: "https://www.ebay.com/b/Apple-iPhone/9355/bn_319682" 
+    url: "https://www.ebay.com/sch/i.html?_nkw=#{ URI.escape(@search) }" 
 }
